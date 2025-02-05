8cef63f1a4b9a4a9ec5c6be5edd16d25cf073c37
Faysal2123 <foysal2005n1@gmail.com>
2025-02-06 01:10:40 +0600

    Implement form handling with React Hook Form in Register page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import LoginAnimation from "../assets/lottie/Animation2.json"
+import Lottie from "react-lottie-player";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
+    
+      const onSubmit = (data) => console.log(data)
   return (
-    <div>
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form className="card-body">
+    <div className='flex md:flex-row flex-col justify-center md:pt-20 pt-6 '>
+         <div className="flex justify-center">
+             <Lottie loop animationData={LoginAnimation} play className='md:h-[400px] md:w-[400px] h-[200px] w-[200px]' />
+                
+        </div>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl justify-center mx-auto md:mx-0">
+       <h1 className="text-4xl font-bold text-center pt-5">Login</h1>
+        <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
+            {...register("email")}
               type="email"
               placeholder="email"
               className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent"
@@ -21,6 +40,7 @@ const Login = () => {
               <span className="label-text">Password</span>
             </label>
             <input
+              {...register("password")}
               type="password"
               placeholder="password"
               className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent"
@@ -29,8 +49,11 @@ const Login = () => {
             
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-accent w-full">Login</button>
           </div>
+          <p className="text-center pt-3 mb-3"><small>Don't have an account</small><Link className="text-red-500 ml-1" to='/register'>Register now</Link></p>
+          <div className="divider">OR</div>
+          <button type="button"  className="btn btn-outline btn-accent "><FaGoogle size={20}/></button>
         </form>
       </div>
     </div>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,44 +1,53 @@
 import React from 'react';
 import { FaGoogle } from 'react-icons/fa';
-import LoginAnimation from '../assets/lottie/Animation1.json'
+import RegisterAnimation from '../assets/lottie/Animation1.json'
 import Lottie from 'react-lottie-player';
 import { Link } from 'react-router-dom';
-
+import { useForm } from 'react-hook-form';
 
 
 const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
+    
+      const onSubmit = (data) => console.log(data)
     return (
         <div className='flex md:flex-row flex-col justify-center md:pt-20 pt-6'>
              <div className='flex justify-center'>
-                <Lottie loop animationData={LoginAnimation} play className='md:h-[500px] md:w-[400px] h-[200px] w-[200px]' />
+                <Lottie loop animationData={RegisterAnimation} play className='md:h-[500px] md:w-[400px] h-[200px] w-[200px]' />
                 </div>
-            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto md:mx-0">
                
-      <form className="card-body">
+      <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
             <h1 className='text-4xl font-bold text-center pb-5'>Register Now</h1>
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="text" placeholder="Your Name" className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
+          <input {...register("name")} type="text" placeholder="Your Name" className="
+          input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Photo</span>
           </label>
-          <input type="text" placeholder="Photo URL" className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
+          <input {...register("photo")} type="text" placeholder="Photo URL" className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
+          <input {...register("email")} type="email" placeholder="email" className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
+          <input {...register("password")} type="password" placeholder="password" className="input input-bordered rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent" required />
          
         </div>
         <div className="form-control mt-6">
